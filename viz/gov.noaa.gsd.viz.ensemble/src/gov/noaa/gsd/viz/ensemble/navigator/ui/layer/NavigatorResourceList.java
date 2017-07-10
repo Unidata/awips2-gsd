88a33fcad054fe75cf2ee821d51458ea61907f20
Evan Polster <evan.polster@noaa.gov>
2017-07-10 15:20:52 -0600

    VLab Issue #29296; Contour control feature plus clean up from refactor of 19443; fixes #29296
    Change-Id: I0b0e0e0b90b494787e405593a554a6a7249b30c5

--- viz/gov.noaa.gsd.viz.ensemble/src/gov/noaa/gsd/viz/ensemble/navigator/ui/layer/NavigatorResourceList.java
+++ viz/gov.noaa.gsd.viz.ensemble/src/gov/noaa/gsd/viz/ensemble/navigator/ui/layer/NavigatorResourceList.java
@@ -33,15 +33,21 @@ import gov.noaa.gsd.viz.ensemble.display.common.GeneratedGridResourceHolder;
 import gov.noaa.gsd.viz.ensemble.display.common.GeneratedTimeSeriesResourceHolder;
 import gov.noaa.gsd.viz.ensemble.display.common.GridResourceHolder;
 import gov.noaa.gsd.viz.ensemble.display.common.HistogramGridResourceHolder;
+import gov.noaa.gsd.viz.ensemble.display.control.contour.ContourControlCapability;
 import gov.noaa.gsd.viz.ensemble.display.rsc.GeneratedEnsembleGridResource;
 import gov.noaa.gsd.viz.ensemble.display.rsc.histogram.HistogramResource;
 import gov.noaa.gsd.viz.ensemble.display.rsc.timeseries.GeneratedTimeSeriesResource;
 import gov.noaa.gsd.viz.ensemble.util.Utilities;
 
 /**
- * Access and operate the Ensemble Tool resources. Any resources contained in
- * this list class will be associated with an editor and is assumed to be
- * controlled by the Ensemble Tool.
+ * A list which holds Ensemble Tool resources. Any resources contained in this
+ * list class will be associated with an ensemble tool layer of which there is
+ * only one ensemble tool layer per editor. It is assumed this list is used to
+ * allow the user to see what resources are loaded, into the ensemble tool, in
+ * the ensemble viewer.
+ * 
+ * Offers accessor methods to get different flavors (generated, ensemble,
+ * individual, tool, etc) of resources.
  * 
  * <pre>
  * 
@@ -49,10 +55,8 @@ import gov.noaa.gsd.viz.ensemble.util.Utilities;
  * 
  * Date         Ticket#    Engineer    Description
  * ------------ ---------- ----------- --------------------------
- * Nov 17, 2014    5056     jing        Initial creation
- * Jan 17, 2016    13211    polster     Renamed GenericResourceHolder 
- *                                      to AbstractResourceHolder
- * Feb 08  2016    13211    polster     Use diamond operator to reduce verbosity
+ * May 17  2017   19443     polster     Initial creation
+ * Jun 27  2017   19325     jing        Added contour capability.
  * 
  * </pre>
  * 
@@ -94,7 +98,7 @@ public class NavigatorResourceList extends ResourceList {
 
         boolean added = super.add(rsc);
         if (added) {
-            configureResourceForEnsembleTool(rsc);
+            configureResourceForEnsembleTool(rscHolder);
             addResourceHolderForEnsembleTool(rscHolder);
             /* no need to repopulate on refresh */
             theToolLayer.forceRefresh(false);
@@ -120,6 +124,16 @@ public class NavigatorResourceList extends ResourceList {
         rscHolder.getRsc().registerListener(rscHolder);
 
         addToProductList(rscHolder);
+
+        /*
+         * Make sure we turn off the image load capability for generated
+         * resources
+         */
+        if (rscHolder.isGenerated() && rscHolder.getRsc()
+                .hasCapability(DisplayTypeCapability.class)) {
+            rscHolder.getRsc().getCapability(DisplayTypeCapability.class)
+                    .setSuppressingMenuItems(true);
+        }
 
         /* Change associated with repository AWIPS2_GSD Issue #29206. */
         if (rscHolder.getRsc() instanceof AbstractGridResource<?>) {
@@ -285,8 +299,7 @@ public class NavigatorResourceList extends ResourceList {
 
     }
 
-    private void configureResourceForEnsembleTool(
-            AbstractVizResource<?, ?> rsc) {
+    private void configureResourceForEnsembleTool(AbstractResourceHolder rh) {
 
         /*
          * The matrix navigator, if open, will be listening for incoming
@@ -296,13 +309,13 @@ public class NavigatorResourceList extends ResourceList {
                 .getToolMode() == EnsembleTool.EnsembleToolMode.MATRIX) {
             for (IResourceRegisteredListener listener : theToolLayer
                     .getResourceRegisteredListeners()) {
-                listener.resourceRegistered(rsc);
+                listener.resourceRegistered(rh.getRsc());
             }
         }
 
         // Set color
-        if (rsc.hasCapability(ColorableCapability.class)) {
-            ColorableCapability colorable = (ColorableCapability) rsc
+        if (rh.getRsc().hasCapability(ColorableCapability.class)) {
+            ColorableCapability colorable = (ColorableCapability) rh.getRsc()
                     .getCapability(ColorableCapability.class);
             RGB color = Utilities.getRandomNiceContrastColor();
             colorable.setColor(color);
@@ -312,9 +325,22 @@ public class NavigatorResourceList extends ResourceList {
          * For now, we suppress the 'Load as Image' menu item from the context
          * sensitive pop-up, for both the Legends and Matrix modes.
          */
-        if (rsc.hasCapability(DisplayTypeCapability.class)) {
-            rsc.getCapability(DisplayTypeCapability.class)
+        if (rh.getRsc().hasCapability(DisplayTypeCapability.class)
+                && !rh.isGenerated()) {
+            rh.getRsc().getCapability(DisplayTypeCapability.class)
                     .setSuppressingMenuItems(false);
+        } else {
+            rh.getRsc().getCapability(DisplayTypeCapability.class)
+                    .setSuppressingMenuItems(true);
+        }
+
+        /*
+         * Add "Contour Control" to grid resource displayed as contour.
+         */
+        if (rh.getRsc() instanceof AbstractGridResource<?>
+                && ((AbstractGridResource<?>) (rh.getRsc()))
+                        .getDisplayType() != DisplayType.IMAGE) {
+            rh.getRsc().getCapability(ContourControlCapability.class);
         }
 
         /*
@@ -327,9 +353,10 @@ public class NavigatorResourceList extends ResourceList {
 
             // Set to default density for all loaded resources if can
             // But it may be unmatched with current display. Fix it later
-            if (rsc.getCapability(DensityCapability.class) != null) {
-                DensityCapability densityCapability = (DensityCapability) rsc
-                        .getCapability(DensityCapability.class);
+            if (rh.getRsc().getCapability(DensityCapability.class) != null
+                    && !rh.isGenerated()) {
+                DensityCapability densityCapability = (DensityCapability) rh
+                        .getRsc().getCapability(DensityCapability.class);
                 densityCapability.setDensity(DEFAULT_DENSITY);
             }
         }
